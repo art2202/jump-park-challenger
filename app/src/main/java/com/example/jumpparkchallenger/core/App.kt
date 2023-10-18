926c65ada77dc6dbd628986d5a3e108f39a38c88
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-10-18 02:21:04 -0300

    feat: add new koin module

--- app/src/main/java/com/example/jumpparkchallenger/core/App.kt
+++ app/src/main/java/com/example/jumpparkchallenger/core/App.kt
@@ -2,12 +2,13 @@ package com.example.jumpparkchallenger.core
 
 import android.app.Application
 import com.example.jumpparkchallenger.core.utils.modules
+import com.example.jumpparkchallenger.core.utils.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application(){
 
-    private val myModule = listOf(modules)
+    private val myModule = listOf(modules, viewModelModule)
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/example/jumpparkchallenger/core/utils/KoinDependencys.kt
+++ app/src/main/java/com/example/jumpparkchallenger/core/utils/KoinDependencys.kt
@@ -4,16 +4,25 @@ import com.example.jumpparkchallenger.data.api.ApiService
 import com.example.jumpparkchallenger.data.api.RestApi
 import com.example.jumpparkchallenger.data.data_source.LoginDataSource
 import com.example.jumpparkchallenger.data.data_source.LoginDataSourceImpl
-import com.example.jumpparkchallenger.data.mapper.LoginDataResponseToLoginMapper
+import com.example.jumpparkchallenger.data.mapper.LoginDataResponseToHomeInfosMapper
 import com.example.jumpparkchallenger.data.repository.LoginRepositoryImpl
 import com.example.jumpparkchallenger.domain.repository.LoginRepository
+import com.example.jumpparkchallenger.domain.usecases.Login
+import com.example.jumpparkchallenger.presentation.login.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val modules = module {
     single<ApiService> { RestApi.getRetrofit().create(ApiService::class.java) }
 
-    single{LoginDataResponseToLoginMapper()}
+    single{LoginDataResponseToHomeInfosMapper()}
 
     single<LoginDataSource> { LoginDataSourceImpl(get()) }
     single<LoginRepository> { LoginRepositoryImpl(get(), get()) }
+
+    single{ Login(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { LoginViewModel(get()) }
 }
