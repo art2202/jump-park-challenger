7526084775eebc6e6cf020ca70195bf5d1a13f0b
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-10-21 19:15:38 -0300

    refactor: change checkin layout

---
+++ app/src/main/java/com/example/jumpparkchallenger/presentation/Color.kt
@@ -0,0 +1,28 @@
+package com.example.jumpparkchallenger.presentation
+
+enum class Color(val nome: String, val hexValue: String) {
+    VERMELHO("Vermelho", "#FF0000"),
+    AZUL("Azul", "#0000FF"),
+    VERDE("Verde", "#008000"),
+    AMARELO("Amarelo", "#FFFF00"),
+    LARANJA("Laranja", "#FFA500"),
+    ROXO("Roxo", "#800080"),
+    ROSA("Rosa", "#FFC0CB"),
+    CINZA("Cinzento", "#808080"),
+    PRETO("Preto", "#000000"),
+    BRANCO("Branco", "#FFFFFF");
+
+    companion object {
+        fun getNomeList(): List<String> {
+            return values().map { it.nome }
+        }
+
+        fun getHexValueByName(nome: String): String? {
+            return values().firstOrNull { it.nome == nome }?.hexValue
+        }
+
+        fun getColorByHexValue(hexValue: String) : String?{
+            return values().firstOrNull { it.hexValue == hexValue }?.nome
+        }
+    }
+}
--- app/src/main/java/com/example/jumpparkchallenger/presentation/checkin/CheckInActivity.kt
+++ app/src/main/java/com/example/jumpparkchallenger/presentation/checkin/CheckInActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jumpparkchallenger.presentation.checkin
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -10,8 +11,10 @@ import com.example.jumpparkchallenger.R
 import com.example.jumpparkchallenger.databinding.ActivityCheckInBinding
 import com.example.jumpparkchallenger.domain.entities.Vehicle
 import com.example.jumpparkchallenger.domain.entities.home.Price
+import com.example.jumpparkchallenger.presentation.Color
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.Date
+import java.util.regex.Pattern
 
 class CheckInActivity : AppCompatActivity() {
 
@@ -38,32 +41,81 @@ class CheckInActivity : AppCompatActivity() {
             setSpinner(it ?: listOf())
         }
         initListeners()
+        setColorSpinner()
     }
 
+    private fun setColorSpinner(){
+        val spinnerData = arrayListOf("selecione...")
+        spinnerData.addAll(Color.getNomeList())
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerData)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerColor.adapter = adapter
+
+    }
     private fun cleanForm() {
-        binding.editTextPlate.setText("")
-        binding.editTextModel.setText("")
-        binding.editTextColor.setText("")
+        binding.editTextPlate.text = ""
+        binding.editTextModel.text = ""
+        binding.spinnerColor.setSelection(0)
         binding.spinner.setSelection(0)
     }
 
     private fun initListeners() {
         binding.buttonConfirm.setOnClickListener {
-
-            val vehicle = Vehicle(
-                binding.editTextPlate.text.toString(),
-                binding.editTextModel.text.toString(),
-                binding.editTextColor.text.toString(),
-                viewModel.prices[binding.spinner.selectedItemPosition],
-                Date()
-            )
-            viewModel.save(vehicle)
+            if(validForm()) {
+                val color = binding.spinnerColor.selectedItem as String
+                val vehicle = Vehicle(
+                    binding.editTextPlate.text,
+                    binding.editTextModel.text,
+                    color,
+                    viewModel.prices[binding.spinner.selectedItemPosition - 1],
+                    Date()
+                )
+                viewModel.save(vehicle)
+            }
         }
     }
 
+    private fun validForm(): Boolean {
+        if(!validPlate(binding.editTextPlate.text)){
+            binding.editTextPlate.error = "Insira uma placa válida"
+            return false
+        }
+
+        if(binding.editTextModel.text.isEmpty()){
+            binding.editTextModel.error = "Insira um modelo"
+            return false
+        }
+
+        val color = binding.spinnerColor.selectedItem as String
+        if(color == "selecione...") {
+            binding.spinnerColor.performClick()
+            Toast.makeText(this, "Selecione uma cor", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        val table = binding.spinner.selectedItem as String
+        if(table == "selecione...") {
+            binding.spinner.performClick()
+            Toast.makeText(this, "Selecione uma tabela", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun validPlate(plate: String): Boolean {
+        if (plate.isEmpty() || plate.length != 7) return false
+        val regex = "[A-Za-z][A-Za-z][A-Za-z][0-9][A-Za-z0-9_-][0-9][0-9]"
+        val pattern = Pattern.compile(regex)
+        val matcher = pattern.matcher(plate)
+        return matcher.matches()
+    }
+
     private fun setSpinner(prices: List<Price>) {
+        val spinnerData = arrayListOf("selecione...")
         val list = prices.map { it.priceType ?: "" }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
+        spinnerData.addAll(list)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerData)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinner.adapter = adapter
     }
